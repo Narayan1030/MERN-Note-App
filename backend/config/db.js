d72ccf656e32b32ccd3d45b8d23db51aa6e9025a
Ayushvishwakarma04 <ayushvishwakarma9625@gmail.com>
2025-10-19 11:39:25 +0530

    Hide MongoDB URL Enhace model Schema Refine Home page UI

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,3 @@ export const connectDB = async ()=>{
       process.exit(1)  //exit with failure
     }
   }
-
-
-  //mongodb+srv://neupanen494_db_user:c7-X%LrCY*T.D_e@cluster0.9xhrnth.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0  //url of mongodb atlas
